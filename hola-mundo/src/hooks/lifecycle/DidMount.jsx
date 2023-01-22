62c3c7f31da5f783a67d23e7cfeebf284e11ab24
ccortesm <ccortesm2000@gmail.com>
2023-01-22 12:38:56 -0500

    DidMount

--- hola-mundo/src/hooks/lifecycle/DidMount.jsx
+++ hola-mundo/src/hooks/lifecycle/DidMount.jsx
@@ -3,3 +3,38 @@
  * de ciclo de vida en componente clase y el hook de ciclo de vida
  * en componente funcional
  */
+
+import React, { Component, useEffect } from 'react';
+
+export class DidMount extends Component {
+
+    componentDidMount (){
+        console.log('Comportamiento antes de que el componente se ananido al DOM')
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>DidMount</h1>
+            </div>
+        );
+    }
+}
+
+
+
+export const DidMountHook = () => {
+    
+    useEffect(() => {
+        console.log('Comportamiento ante de que l componente sea a;adido al DOM');
+    }, [])
+
+    return (
+        <div>
+            <h1>DidMount</h1>    
+        </div>
+    );
+    
+}
+
+
